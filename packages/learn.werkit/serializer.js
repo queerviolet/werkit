297f5b805640ff74d3d778fe3d9f165de086f005
queerviolet <Ashi Krishnan me@ashi.works>
2017-07-29 20:00:16 +0200

    Serializer.

---
+++ packages/learn.werkit/serializer.js
@@ -0,0 +1,48 @@
+const flow = require('rxquire/flow')
+
+const serializer = module.exports = () => {
+  function dispatch(reducer) {
+    dispatch.state =
+      reducer.call(reducer, dispatch.state, reducer)
+    return dispatch
+  }
+  dispatch.state = start()
+  return dispatch
+}
+
+const start = flow({
+  line: '',
+  lines: [],
+  indent: '',
+  levels: [],
+})
+
+const enter = flow(({line, lines, indent}) => ({
+  lines: [...lines, line],
+  line: indent,
+}))
+
+const tab = (by='  ') => flow(({indent, line, levels}) => ({
+  line: by + line,
+  indent: by + indent,
+  levels: [...levels, indent],
+}))
+
+const popTab = flow(({levels, indent}) => ({
+  levels: levels.slice(0, -1),
+  indent: levels[levels.length - 1],
+}), enter)
+
+const appender = ({str}, state) => Object.assign({}, state, {
+  line: state.line + str
+})
+
+const append = (str='') => ({
+  call: appender, str
+})
+
+const toString = ({lines, line}) => [...lines, line].join('\n')
+
+Object.assign(serializer, {
+  start, enter, tab, popTab, append
+})
